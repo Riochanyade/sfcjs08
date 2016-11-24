72fe41be86e7a50b301fe998c28d278d46da68c6
Riochanyade <nittario@net77-dhcp33.sfc.keio.ac.jp>
2016-11-24 12:38:09 +0900

    第８回

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+function play(){
+    let player = document.querySelector("audio");
+    player.play();
+}
+
+function pause(){
+    let player = document.querySelector("audio");
+    player.pause();
+}
+
+function rewind(sec){
+    let player = document.querySelector("audio");
+    player.currentTime = player.currentTime - sec;
+    return sec;
+}
+
+function fastForward(sec){
+    let player = document.querySelector("audio");
+    player.currentTime = player.currentTime + sec;
+    return sec;
+}
+
+let playButton = document.querySelector("#play-button");
+playButton.addEventListener("click,play");
